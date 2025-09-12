17645f69e399c8ed6affd7b3414fcde7600c4688
dorphalsig <dorphalsig@dorphalsig>
2025-09-12 16:24:22 +0200

    Fixed application of plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,11 @@ pluginManagement {
         google()
         mavenCentral()
     }
+    plugins{
+        id("io.gitlab.arturbosch.detekt")     version "1.23.8"   // latest stable on Portal
+        id("de.mannodermaus.android-junit5")  version "1.13.4.0" // latest release
+    }
+
 }
 
 
@@ -16,6 +21,5 @@ dependencyResolutionManagement {
         gradlePluginPortal()
         mavenLocal()
     }
-
 }
 rootProject.name = "testgate"
